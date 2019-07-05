a0cff687e07d548c8262686242b08a761d39e77e
artykalamata <atrykalamata@gmail.com>
2019-07-05 22:26:03 +0200

    Add project files.

---
+++ cSharpMemory/Helper/ProcessFinder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharpMemory.Helper
+{
+    class ProcessFinder
+    {
+        public static Process GetProcessById(int processId)
+        {
+            return Process.GetProcessById(processId);
+        }
+
+        public static Process GetProcessByName(string processName)
+        {
+            return Process.GetProcessesByName(processName).First();
+        }
+
+    }
+}
---
+++ cSharpMemory/Memory/Memory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharpMemory.Memory
+{
+    public class Memory
+    {
+        Process process;
+
+        public Memory(Process process)
+        {
+            this.process = process;
+        }
+
+
+    }
+}
